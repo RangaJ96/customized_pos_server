8a6e407b3c6e59890b8b2ad76f9ec3607897d748
RangaJ96 <madushan8200@gmail.com>
2023-03-12 22:07:05 +0530

    new | customized_pos | v01 | add_spring_data_jpa dependecies and update the springframework version and jdk version

--- src/main/java/com/agrosb/customizedpos/CustomizedPosApplication.java
+++ src/main/java/com/agrosb/customizedpos/CustomizedPosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomizedposApplication {
+public class CustomizedPosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomizedposApplication.class, args);
+		SpringApplication.run(CustomizedPosApplication.class, args);
 	}
 
 }
